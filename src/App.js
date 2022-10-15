61d8da5114410174cd3d1bbe578a4396745a6c4d
ayeasin14 <ayeasin14@gmail.com>
2022-10-15 18:07:22 +0600

    another route added

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="">
       <RegisterReactBooststrap></RegisterReactBooststrap>
-      <RegisterBootstrap></RegisterBootstrap>
+
     </div>
   );
 }
--- src/components/RegisterReactBooststrap.js
+++ src/components/RegisterReactBooststrap.js
@@ -1,14 +1,29 @@
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import app from '../firebase/firebase.init';
+
+const auth = getAuth(app);
+
 
 const RegisterReactBooststrap = () => {
 
     const handleRegister = event => {
+
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.error('error: ', error);
+            });
     }
 
 
@@ -18,12 +33,12 @@ const RegisterReactBooststrap = () => {
             <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control name='email' type="email" placeholder="Enter email" />
+                    <Form.Control name='email' type="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control name='password' type="password" placeholder="Password" />
+                    <Form.Control name='password' type="password" placeholder="Password" required />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
