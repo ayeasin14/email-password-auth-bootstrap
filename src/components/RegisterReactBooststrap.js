2c2cb9351667073a4f1048efdbe3dc2abf695bb4
ayeasin14 <ayeasin14@gmail.com>
2022-10-15 22:09:54 +0600

    error and success message added

--- src/components/RegisterReactBooststrap.js
+++ src/components/RegisterReactBooststrap.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import app from '../firebase/firebase.init';
@@ -8,21 +8,43 @@ const auth = getAuth(app);
 
 
 const RegisterReactBooststrap = () => {
+    const [passwordError, setPasswordError] = useState('');
+    const [success, setSuccess] = useState(false);
 
     const handleRegister = event => {
-
         event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+        setSuccess(false);
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
         console.log(email, password);
+
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setPasswordError('Please provide at least 2 upper case');
+            return;
+        }
+        if (password.length < 6) {
+            setPasswordError('Please should be at least 6 Characters');
+            return;
+        }
+        if (!/(?=.*[!@#$&*])/.test(password)) {
+            setPasswordError('Please add at least one special character')
+            return;
+        }
+        setPasswordError('');
+
 
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 const user = result.user;
                 console.log(user);
+                setSuccess(true);
+                form.reset();
             })
             .catch(error => {
                 console.error('error: ', error);
+                setPasswordError(error.message);
             });
     }
 
@@ -40,10 +62,15 @@ const RegisterReactBooststrap = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control name='password' type="password" placeholder="Password" required />
                 </Form.Group>
+                <p className='text-danger'>{passwordError}</p>
 
+                {
+                    success && <p className='text-success'>User Created Successfully</p>
+                }
                 <Button variant="primary" type="submit">
                     Register
                 </Button>
+
             </Form>
         </div>
     );
